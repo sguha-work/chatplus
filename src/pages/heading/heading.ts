bb01bb2a31d4d009f7c328f18574108c1089b1b6
sguha-work <sguha1988.life@gmail.com>
2017-09-04 18:01:07 +0530

    changes

--- src/pages/heading/heading.ts
+++ src/pages/heading/heading.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {CommonService} from "./../../services/common.service";
-
+import { AfterViewInit } from '@angular/core';
+import * as $ from 'jquery';
 @IonicPage()
 @Component({
   selector: 'page-heading',
   templateUrl: 'heading.html',
 })
-export class HeadingPage {
+export class HeadingPage  implements AfterViewInit{
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private common: CommonService) {
   }
@@ -18,6 +19,21 @@ export class HeadingPage {
 
   public displayContacts() {
     this.common.showPage("page-contacts");
+  }
+
+  public changePhoneNumber() {
+    if(confirm("This will change your default phone number. Proceed?")) {
+      
+    }
+  }
+
+  private adjustStyle() {
+    
+  }
+
+  ngAfterViewInit() {
+    this.adjustStyle();
+    
   }
 
 }
