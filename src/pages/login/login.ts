7dd4bbdfdf80d99dd2780bccf076ccf341d51ace
sguha-work <sguha1988.life@gmail.com>
2017-09-07 15:24:05 +0530

    login work continues

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { CommonService } from './../../services/common.service';
-
+import * as $ from 'jquery';
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
@@ -10,6 +10,33 @@ export class LoginPage {
 
   constructor(public navCtrl: NavController, private common: CommonService) {
   }
+
+  private validate(phn: string, password: string): boolean {
+    let phoneNumberDOM = $("#txt_userPhoneNumber");
+    let passwordDOM = $("#txt_password");
+    if(isNaN(parseInt(phn))) {
+      passwordDOM.css({
+        "border": "1px solide red"
+      });
+      return false;
+    } else {
+      passwordDOM.css({
+        "border": "1px solide green"
+      });
+    }
+
+    if(password === "" || password.length<5) {
+      passwordDOM.css({
+        "border": "1px solide red"
+      });
+      return false;
+    } else {
+      passwordDOM.css({
+        "border": "1px solide green"
+      });
+    }
+    return true;
+  }
   
   public goToSignUpPage() {
     alert("going to signup page");
@@ -17,6 +44,12 @@ export class LoginPage {
   }
 
   public goToLogin() {
-    
+    let phoneNumber = $("#txt_userPhoneNumber").val().trim();
+    let password = $("#txt_password").val().trim();
+    if(this.validate(phoneNumber, password)) {
+      alert("x");
+    } else {
+      return false;
+    }
   }
 }
